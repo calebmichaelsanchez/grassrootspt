40f0edc9ba70e00cbde34d9fec3ab92697984be2
Caleb Sanchez <calebmichaelsanchez@gmail.com>
2017-05-29 15:24:38 -0400

    Adds new product kits

--- js/home/index.js
+++ js/home/index.js
@@ -4,7 +4,7 @@
   if (home) {
     // i hate myself for this
     // I will make this more workable later
-    $("#container").append(`<div id="trailer-01" class="trailer"><div class="trailer__inner"><div class="trailer__magic"><iframe id="video-01" src="https://player.vimeo.com/video/210134747" width="640" height="360" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen="" data-ready="true"></iframe></div></div><a id="close-01" class="close" href="#"></a></div>`);
+    $("#container").append(`<div id="trailer-01" class="trailer"><div class="trailer__inner"><div class="trailer__magic"><iframe id="video-01" width="560" height="315" src="https://www.youtube.com/embed/ylLbqW8s7kI?enablejsapi=1" frameborder="0" allowfullscreen></iframe></div></div><a id="close-01" class="close" href="#"></a></div>`)
     $("#container").append(`<div id="trailer-02" class="trailer"><div class="trailer__inner"><div class="trailer__magic"><iframe id="video-02" width="560" height="315" src="https://www.youtube.com/embed/-WCQaz2O6Uo?enablejsapi=1" frameborder="0" allowfullscreen></iframe></div></div><a id="close-02" class="close" href="#"></a></div>`)
     $("#container").append(`<div id="trailer-03" class="trailer"><div class="trailer__inner"><div class="trailer__magic"><iframe id="video-03" width="560" height="315" src="https://www.youtube.com/embed/lDs1esuqUDs?enablejsapi=1" frameborder="0" allowfullscreen></iframe></div></div><a id="close-03" class="close" href="#"></a></div>`)
     $("#container").append(`<div id="trailer-04" class="trailer"><div class="trailer__inner"><div class="trailer__magic"><iframe id="video-04" width="560" height="315" src="https://www.youtube.com/embed/ApVh_0Qjmck?enablejsapi=1" frameborder="0" allowfullscreen></iframe></div></div><a id="close-04" class="close" href="#"></a></div>`)
@@ -25,24 +25,26 @@
     let iframe02 = $('#video-02');
     let iframe03 = $('#video-03');
     let iframe04 = $('#video-04');
-    let video01 = new Vimeo.Player(iframe01);
+    let video01 = document.getElementById("video-01").contentWindow;
     let video02 = document.getElementById("video-02").contentWindow;
     let video03 = document.getElementById("video-03").contentWindow;
     let video04 = document.getElementById("video-04").contentWindow;
 
-    function vimeoClick(clickId, trailerId, closeId, video) {
-      clickId.click(function(e) {
-        e.preventDefault();
-        trailerId.fadeIn(600);
-        video.play();
-      });
 
-      closeId.click(function(e) {
-        e.preventDefault();
-        video.pause();
-        trailerId.fadeOut(600);
-      });
-    }
+    // save this just in case
+    // function vimeoClick(clickId, trailerId, closeId, video) {
+    //   clickId.click(function(e) {
+    //     e.preventDefault();
+    //     trailerId.fadeIn(600);
+    //     video.play();
+    //   });
+
+    //   closeId.click(function(e) {
+    //     e.preventDefault();
+    //     video.pause();
+    //     trailerId.fadeOut(600);
+    //   });
+    // }
 
     function ytClick(clickId, trailerId, closeId, video) {
       clickId.click(function(e) {
@@ -57,7 +59,7 @@
       });
     }
 
-    vimeoClick(click01, trailer01, close01, video01);
+    ytClick(click01, trailer01, close01, video01);
     ytClick(click02, trailer02, close02, video02);
     ytClick(click03, trailer03, close03, video03);
     ytClick(click04, trailer04, close04, video04);
